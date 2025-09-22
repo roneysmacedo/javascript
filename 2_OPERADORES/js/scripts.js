4978adec21fba155d1c710f0bedc7fabbec40a94
roneysmacedo <rsminfo2018@gmail.com>
2025-09-22 15:07:27 -0400

    Treinamento 10

---
+++ 2_OPERADORES/js/scripts.js
@@ -0,0 +1,30 @@
+// 1 -Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -27);
+
+// 2 - Operacoes aritimeticas
+console.log(5+6);
+console.log(5-6);
+console.log(5*6);
+console.log(5/6);
+
+// 3 - Special Number
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(12 * "asd");
+console.log(typeof NaN);
+
+// 4 - Strings
+console.log("Ola jovem");
+console.log(`Certo jovem`);
+console.log(`1`);
+
+console.log(typeof "Ola jovem");
+console.log(typeof `Certo jovem`);
+console.log(typeof `1`);
+
+// 5 - Simbolos especiais em string
+console.log("Ola jovem \n tudo certo");
+console.log(`Certo jovem \n tem coragem`);
+console.log(`1 \n 2`);
